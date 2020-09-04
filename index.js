8ec61090c70168ae31762aaf124480b376b039b6
adithian-ibots <adithian-ibots@users.noreply.github.com>
2020-09-04 06:45:10 +0530

    Adding the files

---
+++ index.js
@@ -0,0 +1,114 @@
+//------------------------------------------------------------------------------------------------
+// console.log("Summa printing from other file");
+
+// var name="Adithian";
+// var num=2;
+// console.log(name);
+// console.log(typeof name)
+// console.log(typeof num)
+
+// //Arithmetic function
+
+// let num1=5
+// let num2=2
+
+// console.log(num1+num2);
+// console.log(num1-num2);
+// console.log(num1*num2);
+// console.log(num1/num2);
+// console.log(num1%num2);
+// console.log(num1**num2);
+
+//------------------------------------------------------------------------------------------------
+// //Strings
+// let weapon='StormBreaker';
+// let hero='thor';
+
+// // Concat type 1
+// let data1=hero+" has "+weapon;
+// console.log(data1);
+// //Concat type 2 #notworking
+// let data='${hero}';
+// console.log(data);
+
+// // length
+// console.log("Weapon Length is "+weapon.length);
+
+// //replace
+// console.log(weapon.replace("r","a"))
+
+// //index
+// let index=weapon.indexOf("r");
+// console.log(index);
+
+// //Substring(Second Element is count)
+//  let sub=weapon.substr(1,4);
+//  console.log(sub);
+//  let sub2=weapon.substr(weapon.length-3,3);
+//  console.log(sub2);
+
+//  //print using index
+//  let data2="hello";
+//  console.log(data2[1]);
+// console.log(data2.substr(1,data2.length));
+
+//------------------------------------------------------------------------------------------------
+// // Arrays
+// //Collection of Elements of same type
+// let data=["laptop","Monitor","switch","Keyboard"];
+// let numbers=[23,33,45,67]
+// console.log("Full Array is",data)
+// //Accessing Array element using index
+// console.log("First element is "+data[0]);
+// //Accessing array element's element
+// console.log("Second Element of First element of array is "+data[0][1]);
+// //Array length
+// console.log("Length of Second Array is "+numbers.length);
+// //Array slicing (Second arguement is index and not count)
+// console.log(data.slice(0,2))
+// console.log(data.slice(1,3))
+
+//------------------------------------------------------------------------------------------------
+//Control Statements
+// loops & Conditions
+//types
+//while
+// do while
+// for
+
+//for-notes
+// for(let i=1;i<10;i++)
+// {
+//     console.log("hello");
+// }
+
+// while-notes
+// let i=1;
+// while(i<10)
+// {
+//     console.log(i);
+//     i++;
+// }
+
+//do-while notes
+let i=1;
+do{
+    console.log(i);
+    i++
+}
+while(i<=10)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
